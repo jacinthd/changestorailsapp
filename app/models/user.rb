5cacc8c9a66f9c227416bf01f0749a648c4faa1b
raulamit <amit.c.raul@gmail.com>
2014-06-01 05:30:47 -0700

    error trial 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
    
   validates :contact_number, :presence => true, :numericality => true, :length => {:minimum => 10, :maximum => 11}, :uniqueness => true
   #validates :first_name, :presence => true
-  validates :last_name, :presence => true
+  #validates :last_name, :presence => true
   validates :address_line1, :presence => 'true'
   validates :pincode, :presence => 'true', :numericality => true, :length => {:minimum => 6, :maximum => 6}
   validates :city, :presence => 'true'
